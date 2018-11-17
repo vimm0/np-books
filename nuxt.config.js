5c84798eb23506eb08592bfb276d12374a3de781
vimm0 <vimmrana0@gmail.com>
2018-11-17 20:38:30 +0545

    add meta for google search

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ module.exports = {
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'google-site-verification', content: 'A6wVWRUjVgXQaKG1DVaWJeDlFWvriLv5fzvl53uPZUM'},
       {hid: 'description', name: 'description', content: pkg.description}
     ],
     link: [
