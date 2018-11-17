1d9f50827e0faf73dde70b331957e38a4eec3984
vimm0 <vimmrana0@gmail.com>
2018-11-17 20:54:50 +0545

    facebook share

--- pages/index.vue
+++ pages/index.vue
@@ -37,6 +37,13 @@
               </ul>
             </div>
           </div>
+          <a :href="`https://www.facebook.com/sharer/sharer.php?u=${url}&amp;src=sdkpreparse`"
+             target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path
+                d="M12 0a12 12 0 1 0 0 24 12 12 0 0 0 0-24zm3 8h-1.35c-.54 0-.65.22-.65.78V10h2l-.2 2H13v7h-3v-7H8v-2h2V7.7c0-1.78.93-2.7 3.03-2.7H15v3z"/>
+            </svg>
+          </a>
           <footer class="card-footer">
             <div
               class="card-footer-item"
@@ -71,14 +78,18 @@
           // { icon: 'cellphone-link', title: 'Responsive', content: `<span><b class="has-text-grey">Every</b> component is responsive</span>` },
           // { icon: 'alert-decagram', title: 'Modern', content: `<span>Built with <a href="https://vuejs.org/">Vue.js</a> and <a href="http://bulma.io/">Bulma</a></span>` },
           // { icon: 'arrange-bring-to-front', title: 'Lightweight', content: `<span>No other internal dependency</span>` }
-        ]
+        ],
+        url: process.env.HOST_URL + this.$route.path
       }
     }, head() {
       return {
         title: 'Home',
         titleTemplate: '%s | Shikshalaya',
         __dangerouslyDisableSanitizers: ['script'],
-        script: [{innerHTML: JSON.stringify(this.structuredData), type: 'application/ld+json'}]
+        script: [{innerHTML: JSON.stringify(this.structuredData), type: 'application/ld+json'}],
+        meta: [{
+          'og:url': this.url || '',
+        }]
       }
     },
   }
